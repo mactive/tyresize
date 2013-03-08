07bb75afd519ff9a32a212b4da885a2fe4f91ce6
mactive <mactive@gmail.com>
2013-03-08 12:50:50 +0800

    init tyresize

---
+++ tyresize/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  tyresize
+//
+//  Created by meng qian on 13-3-8.
+//  Copyright (c) 2013年 thinktube. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ tyresize/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tyresize
+//
+//  Created by meng qian on 13-3-8.
+//  Copyright (c) 2013年 thinktube. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
